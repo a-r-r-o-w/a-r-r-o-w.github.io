fec9872f640e6e4e93a7e0c65523542642a02943
a-r-r-o-w <avs050602@gmail.com>
2023-10-18 17:50:03 +0530

    improvement: add more information on to website

---
+++ js/script.js
@@ -0,0 +1,52 @@
+let is_dark_mode = true;
+const toggleButton = document.getElementById("toggle-button");
+const body = document.body;
+
+toggleButton.addEventListener("click", () => {
+  is_dark_mode = !is_dark_mode;
+  body.classList.toggle("dark-mode");
+  body.classList.toggle("light-mode");
+});
+
+// create a function load_projects that sends a request to /projects/index.json and parses the response as JSON
+const load_projects = () => {
+  console.log("load_projects");
+  const projectsDiv = document.getElementById("projects");
+
+  fetch("/projects/index.json")
+    .then(response => response.json())
+    .then(json => json["projects"])
+    .then(projects => {
+      projects.forEach((project, index) => {
+        const projectDiv = document.createElement("div");
+        projectDiv.classList.add("cards__project");
+
+        // let displayImg = null;
+        
+        // if (project["image"])
+        //   displayImg = `<img src='${project["image"]}' alt='${project["name"]}' />`;
+        // else {
+        //   console.log(index);
+        //   if (index % 2 == 0)
+        //     displayImg = `<img class="missing-image-gradient-45" />`;
+        //   else
+        //     displayImg = `<img class="missing-image-gradient-135" />`;
+        // }
+
+        const displayImg = project.image
+        ? `<img src="${project.image}" alt="${project.name}" />`
+        : `<img class="missing-image-gradient" style="--gradient-angle: ${index % 2 === 0 ? '45deg' : '135deg'};" />`;
+
+        projectDiv.innerHTML = `
+        <a href='${project["url"]}'>
+          ${displayImg}
+          <div class="cards__text">
+            <h2>${project["name"]}</h2>
+            <p>${project["short_description"]}</p>
+          </div>
+        </a>
+        `;
+        projectsDiv.appendChild(projectDiv);
+      });
+    });
+};
